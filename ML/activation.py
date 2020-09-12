71427bfa8918de7f3d5991df6a3e36e754a2c4bd
Othmane Belhoussine <Othmane.belhou@gmail.com>
2020-09-12 23:28:31 +0100

    Added Requirements + Fixes

--- ML/activation.py
+++ ML/activation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Logistic activation function (scaling numbers between 0 and 1 non-linearly)
+# Logistic a    ctivation function (scaling numbers between 0 and 1 non-linearly)
 def sigmoid(layer):
     return 1/(1 + np.exp(-layer))
 
